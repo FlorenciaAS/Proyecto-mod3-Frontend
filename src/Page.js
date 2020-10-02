ca7c3424a576cf86110af8d2a38621a1b6a2dcf2
Flor Alcaide <natashaflorencia109@gmail.com>
2020-10-02 11:23:40 -0300

    navbar

---
+++ src/Page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Table from './components/Table';
+
+
+function Page() {
+  return (
+    <div className="App">
+      <Navbar/>
+       <Table/>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AppBar } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+
+
+function Navbar() {
+  return (
+    <div className="Navbar-container">
+      <AppBar position="static" className="Navbar-container">
+          <Toolbar className="Toolbar-container">
+              <Box fontFamily="Righteous"
+                    fontSize={28}
+                    align="center"
+                    className="Box-container">
+                        Mateify
+              </Box>
+          </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
