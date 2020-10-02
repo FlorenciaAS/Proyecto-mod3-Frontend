7a7d2eb212d4c90ab5b02cc7e3a113f0d5b5dec0
Flor Alcaide <natashaflorencia109@gmail.com>
2020-10-02 11:32:22 -0300

    table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-
-function Table() {
+function TableComp() {
   return (
     <div>
-      
-    </div>
+       <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Nombre</TableCell>
+            <TableCell align="right">Apellido</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Age</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          
+            <TableRow >
+              <TableCell component="th" scope="row">
+               
+              </TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+        
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+</div>
   );
 };
 
-export default Table;
+export default TableComp;
